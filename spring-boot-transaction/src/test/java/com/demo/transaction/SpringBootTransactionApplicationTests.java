b9f8b1fda86f2a5e7df430d817fc639189c5b911
FangShanShanFamily <643915247@qq.com>
2020-03-26 00:29:37 +0800

    save animal table

--- spring-boot-transaction/src/test/java/com/demo/transaction/SpringBootTransactionApplicationTests.java
+++ spring-boot-transaction/src/test/java/com/demo/transaction/SpringBootTransactionApplicationTests.java
@@ -1,13 +1,29 @@
 package com.demo.transaction;
 
+import com.demo.transaction.model.TransAnimal;
+import com.demo.transaction.service.ITransactionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class SpringBootTransactionApplicationTests {
 
+
+    @Autowired
+    private ITransactionService transactionService;
+
     @Test
     void contextLoads() {
+        TransAnimal transAnimal = new TransAnimal();
+        transAnimal.setAnimalType(1);
+        transAnimal.setAnimalTypeName("陆地动物");
+        transAnimal.setCreateTime(new Date());
+        transAnimal.setUpdateTime(new Date());
+        boolean flag =  transactionService.save(transAnimal);
+
     }
 
 }
