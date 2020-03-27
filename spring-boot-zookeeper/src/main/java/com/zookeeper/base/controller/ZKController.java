49661f296b28430ba33de6d557a00f940a80c27d
fangshanshan@lexue.com <fangshanshan@lexue.com>
2020-03-27 19:55:25 +0800

    zookeeper 功能添加

---
+++ spring-boot-zookeeper/src/main/java/com/zookeeper/base/controller/ZKController.java
@@ -0,0 +1,10 @@
+package com.zookeeper.base.controller;
+
+/**
+ * @ClassName ZKController
+ * @Author FangShanShan
+ * @Description ZK测试
+ * @Date 2020/3/27 19:40
+ */
+public class ZKController {
+}
---
+++ spring-boot-zookeeper/src/test/java/com/zookeeper/base/SpringBootZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zookeeper.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
